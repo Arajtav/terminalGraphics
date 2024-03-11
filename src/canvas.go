a6e3d5149aacf93aaa1e80c518c1e941fd19bbc4
Arajtav <arajtav@gmail.com>
2024-03-11 23:03:37 +0100

    temporary line clipping, fix in printing canvas with odd y size, better checks in SetPixel

--- src/canvas.go
+++ src/canvas.go
@@ -39,7 +39,7 @@ func (s *Canvas) Clear() {
 
 // Prints Canvas to the terminal
 func (s *Canvas) Print() {
-    for i := int16(0); i<s.sizeY; i+=2 {
+    for i := int16(0); i<s.sizeY-1; i+=2 {
         for j := int16(0); j<s.sizeX; j++ {
             fmt.Printf("\033[48;2;%d;%d;%dm\033[38;2;%d;%d;%dm▄", s.data[i][j].R, s.data[i][j].G, s.data[i][j].B, s.data[i+1][j].R, s.data[i+1][j].G, s.data[i+1][j].B);
         }
@@ -49,8 +49,11 @@ func (s *Canvas) Print() {
 
 // Sets value of a pixel at given position to c
 func (s *Canvas) SetPixel(p Vec2, c Color) {
-    if p.X >= float32(s.sizeX/2) || p.Y >= float32(s.sizeY/2) { return; }
-    s.data[roundF32ToI32(p.Y)+int32(s.sizeY/2)][roundF32ToI32(p.X)+int32(s.sizeX/2)] = c;
+    x := roundF32ToI32(p.X);
+    y := roundF32ToI32(p.Y);
+    if x >=  int32(s.sizeX/2)   || y >=  int32(s.sizeY/2)   { return; }
+    if x <= -int32(s.sizeX/2)-1 || y <= -int32(s.sizeY/2)-1 { return; }
+    s.data[y+int32(s.sizeY/2)][x+int32(s.sizeX/2)] = c;
 }
 
 // Same as SetPixel(p, c), but it doesn't check if pixel position is correct
@@ -60,14 +63,12 @@ func (s *Canvas) SetPixelUnsafe(p Vec2, c Color) {
 
 // Draws line from point a to point b
 func (s *Canvas) DrawLine(a Vec2, b Vec2, c Color) {
-    // TODO: CLIP LINE INSTEAD NOT DRAWING IT
-    if a.X >= float32(s.sizeX/2) || a.Y >= float32(s.sizeY/2) || b.X >= float32(s.sizeX/2) || b.Y >= float32(s.sizeY/2) { return; }
-    if a.X < float32(-s.sizeX/2) || a.Y < float32(-s.sizeY/2) || b.X < float32(-s.sizeX/2) || b.Y < float32(-s.sizeY/2) { return; }
+    // TODO: REAL LINE CLIPPING INSTEAD RELYING ON SetPixel SAFETY
     ll := dist2D(a, b);
     for t := float32(0); t<=1.0; t+=(1/ll) {
-        s.SetPixelUnsafe(Vec2{Interpolate(a.X, b.X, t), Interpolate(a.Y, b.Y, t)}, c);
+        s.SetPixel(Vec2{Interpolate(a.X, b.X, t), Interpolate(a.Y, b.Y, t)}, c);
     }
-    s.SetPixelUnsafe(b, c);
+    s.SetPixel(b, c);
 }
 
 // Outline of triangle
