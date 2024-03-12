5e13dec4369229ffdd100722ad7e2ec6796cac0f
Arajtav <arajtav@gmail.com>
2024-03-12 11:44:34 +0100

    camera space, rotation around camera, camera position

--- examples/example.go
+++ examples/example.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	tg "terminalGraphics/src"
+    "fmt"
+    tg "terminalGraphics/src"
+    "time"
 )
 
 func main() {
@@ -9,17 +11,30 @@ func main() {
     var s tg.Canvas;
     s.Init(96, 96);
 
-    fv := float32(32.0);
+    cube1 := tg.Cube();
 
-    c := tg.Color{0, 0, 0};
+    cube2 := tg.Cube();
+    cube2.SetScale(tg.Vec3{2.0, 2.0, 2.0});
+    cube2.SetRotation(tg.Vec3{tg.DegreesToRadians(45.0), tg.DegreesToRadians(45.0), 0})
 
-    cube := tg.Cube();
-    cube.Position.Z = 3.0;
+    w := tg.GetEmptyWorld(96.0);
+
+    w.AddModel(&cube1);
+    w.AddModel(&cube2);
+
+    w.Cam.Pos.Z = -8.0;
+
+
     for i := 0; true; i++ {
+        ts := time.Now().UnixNano();
         s.Fill(tg.Color{255, 255, 255});
-        cube.SetRotation(tg.Vec3{tg.DegreesToRadians(float32(2*i%360)), tg.DegreesToRadians(float32(5*i%360)), 0.0});
-        tg.DrawModel(&s, cube, fv, c);
+        cube1.SetRotation(tg.Vec3{tg.DegreesToRadians(float32(2*i%360)), tg.DegreesToRadians(float32(5*i%360)), 0.0});
+        w.Render(&s, tg.Color{0, 0, 0});
+        w.Cam.Rot.Z += tg.DegreesToRadians(1.0);
         tg.Chome();
+        fmt.Println();
         s.Print();
+        tg.Chome();
+        fmt.Printf("current fps: %f", 1000000000/float64(time.Now().UnixNano()-ts));
     }
 }
--- src/utils3d.go
+++ src/utils3d.go
@@ -1,40 +1,69 @@
 package terminalGraphics
 
+// TODO: ACTUALLY USE CAMERA
+type Camera struct {
+    Fv float32;
+    Rot Vec3;
+    Pos Vec3;
+}
+
+type World struct {
+    models []*Model;
+    Cam Camera;
+}
+
+func GetEmptyWorld(fv float32) World {
+    var w World;
+    w.Cam.Fv = fv;
+    return w;
+}
+
+func (w *World) AddModel(m *Model) {
+    w.models = append(w.models, m);
+}
+
+func (w *World) Render(s *Canvas, c Color) {
+    for i := 0 ; i<len(w.models); i++ {
+        for j := 0; j<len(w.models[i].triangles); j++ {
+            // calculate Vertex position in world space
+            v0 := AddVec3(w.models[i].vertices[w.models[i].triangles[j].i0], AddVec3(w.models[i].Position, w.Cam.Pos));
+            v1 := AddVec3(w.models[i].vertices[w.models[i].triangles[j].i1], AddVec3(w.models[i].Position, w.Cam.Pos));
+            v2 := AddVec3(w.models[i].vertices[w.models[i].triangles[j].i2], AddVec3(w.models[i].Position, w.Cam.Pos));
+            v0 = Rotate3D(v0, w.Cam.Rot);
+            v1 = Rotate3D(v1, w.Cam.Rot);
+            v2 = Rotate3D(v2, w.Cam.Rot);
+
+            drawTriangle3D(s, v0, v1, v2, w.Cam.Fv, c);
+        }
+    }
+}
+
 // converts 3D position to position on screen
-func Point3DToPoint2D(v Vec3, fv float32) Vec2 {
+func point3DToPoint2D(v Vec3, fv float32) Vec2 {
     return Vec2{v.X/v.Z*fv, v.Y/v.Z*fv};
 }
 
-func DrawPoint3D(s *Canvas, v Vec3, fv float32, c Color) {
-    s.SetPixel(Point3DToPoint2D(v, fv), c);
+func drawPoint3D(s *Canvas, v Vec3, fv float32, c Color) {
+    s.SetPixel(point3DToPoint2D(v, fv), c);
 }
 
 // Line from Vec3 to Vec3
-func DrawLine3D(s *Canvas, a Vec3, b Vec3, fv float32, c Color) {
-    s.DrawLine(Point3DToPoint2D(a, fv), Point3DToPoint2D(b, fv), c)
+func drawLine3D(s *Canvas, a Vec3, b Vec3, fv float32, c Color) {
+    s.DrawLine(point3DToPoint2D(a, fv), point3DToPoint2D(b, fv), c)
 }
 
-func DrawTriangle3D(s *Canvas, v0 Vec3, v1 Vec3, v2 Vec3, fv float32, c Color) {
-    s.DrawTriangle(Point3DToPoint2D(v0, fv), Point3DToPoint2D(v1, fv), Point3DToPoint2D(v2, fv), c);
-}
-
-func DrawModel(s *Canvas, m Model, fv float32, c Color) {
-    for i := 0; i<len(m.triangles); i++ {
-        // calculate Vertex position in world space
-        v0 := AddVec3(m.vertices[m.triangles[i].i0], m.Position);
-        v1 := AddVec3(m.vertices[m.triangles[i].i1], m.Position);
-        v2 := AddVec3(m.vertices[m.triangles[i].i2], m.Position);
-        DrawTriangle3D(s, v0, v1, v2, fv, c);
-    }
+func drawTriangle3D(s *Canvas, v0 Vec3, v1 Vec3, v2 Vec3, fv float32, c Color) {
+    s.DrawTriangle(point3DToPoint2D(v0, fv), point3DToPoint2D(v1, fv), point3DToPoint2D(v2, fv), c);
 }
 
 // For debug, renders only points
-func DrawModelV(s *Canvas, m Model, fv float32, c Color) {
+func drawModelV(s *Canvas, m Model, fv float32, c Color) {
     for i := 0; i<len(m.vertices); i++ {
-        DrawPoint3D(s, m.vertices[i], fv, c);
+        drawPoint3D(s, m.vertices[i], fv, c);
     }
 }
 
+// Adds x of v0 to v1, y to y and so on
 func AddVec3(v0 Vec3, v1 Vec3) Vec3 {
     v0.X += v1.X;
     v0.Y += v1.Y;
