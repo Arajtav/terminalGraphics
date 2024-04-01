d78cdd16d28c15528dcbc644f862aa4950ddeb1b
Arajtav <arajtav@gmail.com>
2024-04-02 01:13:32 +0200

    better uv in cube primitive

--- src/primitives.go
+++ src/primitives.go
@@ -7,23 +7,29 @@ import (
 func Cube(mt tc.Material) Model {
     m := GetEmptyModel();
 
+    // bottom
     m.AddTriangle(Vertex{Vec3{ 1,  1,  1}, Vec2{1, 1}}, Vertex{Vec3{-1,  1,  1}, Vec2{0, 1}}, Vertex{Vec3{ 1,  1, -1}, Vec2{1, 0}}, mt);
     m.AddTriangle(Vertex{Vec3{-1,  1, -1}, Vec2{0, 0}}, Vertex{Vec3{-1,  1,  1}, Vec2{0, 1}}, Vertex{Vec3{ 1,  1, -1}, Vec2{1, 0}}, mt);
 
-    m.AddTriangle(Vertex{Vec3{ 1, -1,  1}, Vec2{1, 1}}, Vertex{Vec3{ 1,  1,  1}, Vec2{0, 1}}, Vertex{Vec3{ 1, -1, -1}, Vec2{1, 0}}, mt);
-    m.AddTriangle(Vertex{Vec3{ 1,  1, -1}, Vec2{0, 0}}, Vertex{Vec3{ 1,  1,  1}, Vec2{0, 1}}, Vertex{Vec3{ 1, -1, -1}, Vec2{1, 0}}, mt);
+    // right
+    m.AddTriangle(Vertex{Vec3{ 1,  1,  1}, Vec2{1, 1}}, Vertex{Vec3{ 1,  1, -1}, Vec2{0, 1}}, Vertex{Vec3{ 1, -1,  1}, Vec2{1, 0}}, mt);
+    m.AddTriangle(Vertex{Vec3{ 1, -1, -1}, Vec2{0, 0}}, Vertex{Vec3{ 1,  1, -1}, Vec2{0, 1}}, Vertex{Vec3{ 1, -1,  1}, Vec2{1, 0}}, mt);
 
-    m.AddTriangle(Vertex{Vec3{-1, -1,  1}, Vec2{1, 1}}, Vertex{Vec3{ 1, -1,  1}, Vec2{0, 1}}, Vertex{Vec3{-1, -1, -1}, Vec2{1, 0}}, mt);
-    m.AddTriangle(Vertex{Vec3{ 1, -1, -1}, Vec2{0, 0}}, Vertex{Vec3{ 1, -1,  1}, Vec2{0, 1}}, Vertex{Vec3{-1, -1, -1}, Vec2{1, 0}}, mt);
+    // top
+    m.AddTriangle(Vertex{Vec3{ 1, -1, -1}, Vec2{1, 1}}, Vertex{Vec3{-1, -1, -1}, Vec2{0, 1}}, Vertex{Vec3{ 1, -1,  1}, Vec2{1, 0}}, mt);
+    m.AddTriangle(Vertex{Vec3{-1, -1,  1}, Vec2{0, 0}}, Vertex{Vec3{-1, -1, -1}, Vec2{0, 1}}, Vertex{Vec3{ 1, -1,  1}, Vec2{1, 0}}, mt);
 
-    m.AddTriangle(Vertex{Vec3{-1,  1,  1}, Vec2{1, 1}}, Vertex{Vec3{-1, -1,  1}, Vec2{0, 1}}, Vertex{Vec3{-1,  1, -1}, Vec2{1, 0}}, mt);
-    m.AddTriangle(Vertex{Vec3{-1, -1, -1}, Vec2{0, 0}}, Vertex{Vec3{-1, -1,  1}, Vec2{0, 1}}, Vertex{Vec3{-1,  1, -1}, Vec2{1, 0}}, mt);
+    // left
+    m.AddTriangle(Vertex{Vec3{-1,  1, -1}, Vec2{1, 1}}, Vertex{Vec3{-1,  1,  1}, Vec2{0, 1}}, Vertex{Vec3{-1, -1, -1}, Vec2{1, 0}}, mt);
+    m.AddTriangle(Vertex{Vec3{-1, -1,  1}, Vec2{0, 0}}, Vertex{Vec3{-1,  1,  1}, Vec2{0, 1}}, Vertex{Vec3{-1, -1, -1}, Vec2{1, 0}}, mt);
 
-    m.AddTriangle(Vertex{Vec3{ 1,  1, -1}, Vec2{1, 1}}, Vertex{Vec3{ 1, -1, -1}, Vec2{0, 1}}, Vertex{Vec3{-1,  1, -1}, Vec2{1, 0}}, mt);
-    m.AddTriangle(Vertex{Vec3{-1, -1, -1}, Vec2{0, 0}}, Vertex{Vec3{ 1, -1, -1}, Vec2{0, 1}}, Vertex{Vec3{-1,  1, -1}, Vec2{1, 0}}, mt);
+    // front
+    m.AddTriangle(Vertex{Vec3{ 1,  1, -1}, Vec2{1, 1}}, Vertex{Vec3{-1,  1, -1}, Vec2{0, 1}}, Vertex{Vec3{ 1, -1, -1}, Vec2{1, 0}}, mt);
+    m.AddTriangle(Vertex{Vec3{-1, -1, -1}, Vec2{0, 0}}, Vertex{Vec3{-1,  1, -1}, Vec2{0, 1}}, Vertex{Vec3{ 1, -1, -1}, Vec2{1, 0}}, mt);
 
-    m.AddTriangle(Vertex{Vec3{ 1,  1,  1}, Vec2{1, 1}}, Vertex{Vec3{ 1, -1,  1}, Vec2{0, 1}}, Vertex{Vec3{-1,  1,  1}, Vec2{1, 0}}, mt);
-    m.AddTriangle(Vertex{Vec3{-1, -1,  1}, Vec2{0, 0}}, Vertex{Vec3{ 1, -1,  1}, Vec2{0, 1}}, Vertex{Vec3{-1,  1,  1}, Vec2{1, 0}}, mt);
+    // back
+    m.AddTriangle(Vertex{Vec3{-1,  1,  1}, Vec2{1, 1}}, Vertex{Vec3{ 1,  1,  1}, Vec2{0, 1}}, Vertex{Vec3{-1, -1,  1}, Vec2{1, 0}}, mt);
+    m.AddTriangle(Vertex{Vec3{ 1, -1,  1}, Vec2{0, 0}}, Vertex{Vec3{ 1,  1,  1}, Vec2{0, 1}}, Vertex{Vec3{-1, -1,  1}, Vec2{1, 0}}, mt);
 
     return m;
 }
