501dcb56940a93f9fdd568aecfa0a220a3472dce
Arajtav <arajtav@gmail.com>
2024-08-13 01:54:09 +0200

    PrintD in rotating cube example

--- examples/rotatingCube.go
+++ examples/rotatingCube.go
@@ -5,29 +5,43 @@ import (
     tc "github.com/Arajtav/terminalCanvas"
 )
 
+var frames [3]*tc.Canvas
+
+func swapCanvas() {
+    frames[2] = frames[0]
+    frames[0] = frames[1]
+    frames[1] = frames[2]
+}
+
 func main() {
-    tg.ClearTerm();
-    s := *tc.NewCanvas(96, 96);
-    s.Ccolor.B = 255;
+    const canvasSize = 96
+    tg.ClearTerm()
 
-    m := tg.Cube(tc.MaterialUV{});
-    m.SetScale(tg.Vec3{X:2.0, Y:2.0, Z:2.0});
+    // 3, because frames[2] will be used for swapping
+    frames[0] = tc.NewCanvas(canvasSize, canvasSize)
+    frames[1] = tc.NewCanvas(canvasSize, canvasSize)
+    frames[0].Ccolor.B = 255
+    frames[1].Ccolor.B = 255
 
-    w := tg.GetEmptyWorld(96.0);
+    m := tg.Cube(tc.MaterialUV{})
+    m.SetScale(tg.Vec3{X: 2.0, Y: 2.0, Z: 2.0})
 
-    w.Cam.Position.Z = -8.0;
-    w.Cam.Position.Y =  8.0;
-    w.Cam.Rotation.X = tg.DegreesToRadians(45.0);
+    w := tg.GetEmptyWorld(96.0)
 
-    w.AddModel(&m);
+    w.Cam.Position.Z = -8.0
+    w.Cam.Position.Y = 8.0
+    w.Cam.Rotation.X = tg.DegreesToRadians(45.0)
 
+    w.AddModel(&m)
+
+    frames[1].Print() // this is important, because later there is only PrintD
     i := float32(0.0)
     for {
-        s.Clear();
-        w.Render(&s);
-        m.SetRotation(tg.Vec3{X:0, Y:0, Z:tg.DegreesToRadians(i)});
-        tg.Chome();
-        s.Print();
-        i += 1.0;
+        frames[0].Clear()
+        w.Render(frames[0])
+        m.SetRotation(tg.Vec3{X: 0, Y: 0, Z: tg.DegreesToRadians(i)})
+        frames[0].PrintD(frames[1])
+        i += 0.5
+        swapCanvas()
     }
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module terminalGraphics
 
 go 1.22.1
 
-require github.com/Arajtav/terminalCanvas v0.0.0-20240331224407-6158dfd6b7ef
+require github.com/Arajtav/terminalCanvas v0.0.0-20240420112227-1468bbb917d7
