cbd269ff2a7f2366cc6f95e1b9f4e0f58a3d83a5
Arajtav <arajtav@gmail.com>
2024-08-14 00:08:57 +0200

    feat: timer in example

--- examples/rotatingCube.go
+++ examples/rotatingCube.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-    tg "terminalGraphics/src"
-    tc "github.com/Arajtav/terminalCanvas"
+	"fmt"
+	tg "terminalGraphics/src"
+	"time"
+
+	tc "github.com/Arajtav/terminalCanvas"
 )
 
 var frames [3]*tc.Canvas
+var models [5]tg.Model
 
 func swapCanvas() {
     frames[2] = frames[0]
@@ -23,25 +27,37 @@ func main() {
     frames[0].Ccolor.B = 255
     frames[1].Ccolor.B = 255
 
-    m := tg.Cube(tc.MaterialUV{})
-    m.SetScale(tg.Vec3{X: 2.0, Y: 2.0, Z: 2.0})
+    for i := 0; i<len(models); i++ {
+        models[i] = tg.Cube(tc.MaterialUV{})
+        models[i].SetScale(tg.Vec3{X: 1.0, Y: 1.0, Z: 1.0})
+    }
+    models[0].SetScale(tg.Vec3{X: 2.0, Y: 2.0, Z: 2.0})
+    models[1].Position.X =  5
+    models[2].Position.X = -5
+    models[3].Position.Y =  5
+    models[4].Position.Y = -5
 
-    w := tg.GetEmptyWorld(96.0)
+    w := tg.GetEmptyWorld(canvasSize)
 
     w.Cam.Position.Z = -8.0
     w.Cam.Position.Y = 8.0
     w.Cam.Rotation.X = tg.DegreesToRadians(45.0)
 
-    w.AddModel(&m)
+    for i := 0; i<len(models); i++ { w.AddModel(&models[i]) }
 
     frames[1].Print() // this is important, because later there is only PrintD
-    i := float32(0.0)
+    c := float32(0.0)
     for {
         frames[0].Clear()
+        time_start := time.Now().Nanosecond()
         w.Render(frames[0])
-        m.SetRotation(tg.Vec3{X: 0, Y: 0, Z: tg.DegreesToRadians(i)})
+        time_render := time.Now().Nanosecond()
         frames[0].PrintD(frames[1])
-        i += 0.5
+        time_print := time.Now().Nanosecond()
+        for i := 0; i<len(models); i++ { models[i].SetRotation(tg.Vec3{Z: tg.DegreesToRadians(c)}) }
+        time_op := time.Now().Nanosecond()
+        fmt.Printf("render time: %10dns\nprint  time: %10dns\nchange time: %10dns\ntotal  time: %10dns\n", time_render - time_start, time_print - time_render, time_op - time_print, time.Now().Nanosecond() - time_start)
+        c += 0.5
         swapCanvas()
     }
 }
